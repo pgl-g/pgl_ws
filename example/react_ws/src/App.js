044b613d169ad2798839af4680426c87f0bdbd10
顾孝标 <guxiaobiao@xinxuan.net>
2023-11-21 11:40:34 +0800

    feat: example react_ws nodeServer

---
+++ example/react_ws/src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef } from 'react';
+
+import './App.css'
+import Ws from './utils';
+
+function App() {
+
+  const wsRef = useRef(null);
+
+  // const initWs = async () => {
+  //   // 实例化一个WebSocket的对象,要用await，比如重连肯定是异步的，需要返回一个ws的对象
+  //   // create的时候相当于new Ws
+  //   wsRef.current = await Ws.create('ws://localhost:8000')
+  // }
+
+  function wsConnect() {
+    /**
+     * 重连时这里要重新将实例交给ws
+     * 1.发布订阅监听
+     * 2.直接传
+     */
+    wsRef.current = Ws.create('ws://localhost:8000', wsReConnect)
+  }
+
+  function wsReConnect() {
+    // 如果ws不存在，我们就重新创建ws
+    if (!wsRef.current) {
+      return wsConnect()
+    }
+
+    if (wsRef.current && wsRef.current.reconnectingTimer) {
+      clearTimeout(wsRef.current.reconnectingTimer)
+      wsRef.current.reconnectingTimer = null;
+      wsConnect()
+    }
+  }
+
+  const handleSend = () => {
+    // 用户信息,  HEART_BEAT封装在wsRef中
+    wsRef.current.send(JSON.stringify({
+      mode: 'MESSAGE',
+      msg: 'Hello'
+    }))
+  }
+
+  useEffect(() => {
+    // wsConnect()
+  }, [])
+
+
+  return (
+    <div className="App">
+      <button onClick={handleSend}>发送WS</button>
+    </div>
+  );
+}
+
+export default App;
--- example/react_ws/src/utils.js
+++ example/react_ws/src/utils.js
@@ -3,55 +3,42 @@ const WS_MODE = {
   HEART_BEAT: 'HEART_BEAT'
 }
 
+
+// 继承WebSocket
+// 当new Ws的时候，就相当于new一个WebSocket的对象
 class Ws extends WebSocket {
-  wsUrl: string;
-  reconnectingTimer: NodeJS.Timeout | null;
-  heartBeatTimer: NodeJS.Timer | any;
-  wsReConnect: any;
-  constructor(url: string, wsReConnect?: string | string[]) {
-    /**
-     * 继承WebSocket
-     */
-    super(url, wsReConnect);
-    /**
-     * 当进行重新连接的时候，需要重新指定url
-     */
-    this.wsUrl = url;
-    /**
-     * 重连定时器
-     */
-    this.reconnectingTimer = null;
-    /**
-     * 设置心跳时间
-     */
-    this.heartBeatTimer = null;
-    /**
-     * 重连
-     */
-    this.wsReConnect = wsReConnect;
+  // 当new的时候填写的url，相当于在这加一个url,相当于调constructor
+  // url是 ws://localhost:8000
+  constructor(url, wsReConnect) {
+    // 将url交给super，则是WebSocket的constructor
+    super(url)
+    // 当实例化重连的时候，还得需要url,名字不能起this.url,因为WebSocket实例中有url
+    this.wsUrl = url
+    this.heartBeatTimer = null
+    this.reconnectingTimer = null
+    this.wsReConnect = wsReConnect
+    this.init()
   }
-
-  // 初始化页面
-  public init() {
-    try {
-      // this实例来进行监听
-      this.addEventListener('open', this.handleOpen.bind(this), false);
-      this.addEventListener('close', this.handleClose.bind(this), false);
-      this.addEventListener('error', this.handleError.bind(this), false);
-      this.addEventListener('message', this.handleMessage.bind(this), false);
-    } catch (error) {
-      console.log(error, '又是哪里出现问题了')
-    }
+  // init 相当于一个开关
+  init() {
+    this.bindEvent()
   }
-
-  public handleOpen() {
+  // 绑定函数，如open、close、error、message
+  bindEvent() {
+    // this:Ws的实例，监听open、close、error、message
+    this.addEventListener('open', this.handleOpen, false)
+    this.addEventListener('close', this.handleClose, false)
+    this.addEventListener('error', this.handleError, false)
+    this.addEventListener('message', this.handleMessage, false)
+  }
+  handleOpen() {
     console.log('--- Client is connected---')
     // 连接时，开启心跳机制,检查是否断开，断开需要重连
     // 在startHeartBeat肯定要做间隙interval，则在reconnect中需要做延时进行重连，则需要两个计时器
     this.startHeartBeat()
   }
 
-  public handleClose() {
+  handleClose() {
     console.log('--- Client is closed---')
     // 关闭时，如果客户端还在，需要重连，页面关闭就彻底关闭。
     if (this.heartBeatTimer) {
@@ -65,18 +52,21 @@ class Ws extends WebSocket {
       this.reconnectingTimer = null
     }
     this.reconnect()
+
   }
 
-  public handleError(e) {
+  handleError(e) {
     console.log('--- Client is error---', e)
     this.reconnect()
 
   }
 
-  public handleMessage(data) {
+  handleMessage(data) {
     // console.log('--- Client is msg---')
     const { mode, msg } = this.receiveMsg(data)
+
     switch (mode) {
+
       case WS_MODE.MESSAGE:
         console.log('--- MESSAGE ---', msg)
         break;
@@ -87,25 +77,14 @@ class Ws extends WebSocket {
       default:
         break;
     }
-  }
 
-  // 将data换成js对象
-  receiveMsg({ data }) {
-    return JSON.parse(data)
   }
-
-  // 延迟重连
-  reconnect() {
-    this.reconnectingTimer = setTimeout(() => {
-      this.wsReConnect()
-    }, 3000)
-  }
-
-  // 心跳
+  // 计时器
   startHeartBeat() {
     this.heartBeatTimer = setInterval(() => {
       // 告诉服务端，来了一个HEART_BEAT的消息,this.connentStatue为真时，才发送消息，
       // 如果关闭时也发送会报错
+
       if (this.readyState === 1) {
         this.sendMsg({
           mode: WS_MODE.HEART_BEAT,
@@ -117,7 +96,15 @@ class Ws extends WebSocket {
       }
       // 模拟关闭,导致重连reconnect
       this.waitForResponse()
+
     }, 4000)
+  }
+
+  // 延迟重连
+  reconnect() {
+    this.reconnectingTimer = setTimeout(() => {
+      this.wsReConnect()
+    }, 3000)
   }
 
   waitForResponse() {
@@ -126,6 +113,10 @@ class Ws extends WebSocket {
     }, 2000)
   }
 
+  // 将data换成js对象
+  receiveMsg({ data }) {
+    return JSON.parse(data)
+  }
 
   // 发送信息，用字符串
   sendMsg(data) {
@@ -137,7 +128,5 @@ class Ws extends WebSocket {
   }
 
 }
-
-
 
 export default Ws;
